ac926a78a27b72dc2d469bfd6ac17ad2f807f76f
Stien <stien.vanderhallen@hotmail.com>
2020-03-09 15:15:42 +0100

    Migration to VM for real traffic test

--- src/USBtinLibDemo.java
+++ src/USBtinLibDemo.java
@@ -19,15 +19,14 @@ import java.io.IOException;
 public class USBtinLibDemo {
 
     private static final long PERIOD = 200;
-    private static final long DELTA = 10;
+    private static final long DELTA = 7;
     private static final int WINDOW_LENGTH = 4;
     private static final int CHANNEL = 10000;
-    private static final long NOISE_PERIOD = 0; // NOISE_PERIOD=0 --> no noise
-    // private static final long NOISE_PERIOD = 0;
+    private static final long NOISE_PERIOD = 3000; // NOISE_PERIOD=0 --> no noise
 
-    private static final String SENDER_PORT = "/dev/tty.usbmodemA02183211";
+    private static final String SENDER_PORT = "/dev/tty.usbmodemA021CFBA1";
     private static final String RECEIVER_PORT = "/dev/tty.usbmodemA02102821";
-    private static final String NOISE_PORT = "/dev/tty.usbmodemA021CFBA1";
+    private static final String NOISE_PORT = "/dev/tty.usbmodemA02183211";
     private static final int WATCHID = 0x100;
 
     private static final int START_SILENCE = 2;
@@ -50,22 +49,20 @@ public class USBtinLibDemo {
 
     public static void main(String[] args) {
         // Run the transmission thread
-        TRANSMISSION_CHANNEL.addAuthCorrectionCode(AUTH_CORRECTOR);
-        TRANSMISSION_CHANNEL.addAttestationProtocol(AUTH_PROTOCOL);
-        TRANSMISSION_CHANNEL.start();
-
-        // Start physical nodes
+         TRANSMISSION_CHANNEL.addAuthCorrectionCode(AUTH_CORRECTOR);
+         TRANSMISSION_CHANNEL.addAttestationProtocol(AUTH_PROTOCOL);
+         TRANSMISSION_CHANNEL.start();
 
         // Run a noise thread
-        NoiseThread noise = new NoiseThread(NOISE_PERIOD, CHANNEL, NOISE_PORT,
-                new CANMessage(0x200, new byte[]{0x11, 0x22, 0x33}));
-        noise.start();
+//        NoiseThread noise = new NoiseThread(NOISE_PERIOD, CHANNEL, NOISE_PORT,
+//                new CANMessage(0x40, new byte[]{0x11, 0x22, 0x33}));
+//        noise.start();
 
         // End the channel
         try {
             System.in.read();
             TRANSMISSION_CHANNEL.end();
-            noise.end();
+            //noise.end();
         } catch (IOException e) {
             e.printStackTrace();
         }
